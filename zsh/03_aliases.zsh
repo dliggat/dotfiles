e353cec0a0ee0b4160b42f44db044e3d4528f908
Dave Liggat <dliggat@gmail.com>
2015-01-06 15:52:56 -0800

    Add a variables file and a dotfiles command

--- zsh/03_aliases.zsh
+++ zsh/03_aliases.zsh
@@ -7,6 +7,7 @@ alias pgen='pwgen -sy 20'
 alias k9='kill -9'
 alias embergo='npm install && bower install && ember build --watch'
 alias bandwidth="wget --output-document=/dev/null --report-speed=bits http://speedtest.wdc01.softlayer.com/downloads/test10.zip 2>&1 | grep ') -'"
+alias dotfiles="subl ~/git/me/dotfiles"
 
 # Rails aliases.
 alias beg='bundle exec guard'
