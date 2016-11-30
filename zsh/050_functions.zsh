1657a3a9c046134e48ded19d3b1f23671b5f4ff7
Dave Liggat <dave@liggat.org>
2016-11-30 10:01:55 -0600

    Fixing timestamp functions

--- zsh/050_functions.zsh
+++ zsh/050_functions.zsh
@@ -38,12 +38,22 @@ function pg {
 # Get current timestamp. Use option '-c' to copy to clipboard.
 function ts {
   iso_stamp=`date +"%Y-%m-%d %H:%M:%S"`
-  if [ "$1" == "-c" ]; then
-    echo -n $iso_stamp | pbcopy
-    echo "[$iso_stamp] copied to clipboard."
-  else
+  # if [ "$1" == "-c" ]; then
+  #   echo -n $iso_stamp | pbcopy
+  #   echo "[$iso_stamp] copied to clipboard."
+  # else
     echo $iso_stamp
-  fi
+  # fi
+}
+
+function iso {
+  iso_stamp=`date +"%Y-%m-%d"`
+  # if [ "$1" == "-c" ]; then
+  #   echo -n $iso_stamp | pbcopy
+  #   echo "[$iso_stamp] copied to clipboard."
+  # else
+    echo $iso_stamp
+  # fi
 }
 
 function tmpname {
